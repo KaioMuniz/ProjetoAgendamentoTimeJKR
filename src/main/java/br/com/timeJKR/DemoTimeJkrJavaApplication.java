4faef9d0d38987a0015cb330233c197d4233ba28
KaioMuniz <kkaioribeiro@gmail.com>
2025-11-26 21:21:13 -0300

    first commit

---
+++ src/main/java/br/com/timeJKR/DemoTimeJkrJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.timeJKR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTimeJkrJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTimeJkrJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/timeJKR/DemoTimeJkrJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.timeJKR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTimeJkrJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
